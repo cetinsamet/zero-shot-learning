c2e914dbfdd590c605fe8721276d8ff18ba8bbb5
Samet Çetin <e2036432@ceng.metu.edu.tr>
2018-07-19 23:23:55 +0300

    Add feature extractor module

---
+++ src/feature_extractor.py
@@ -0,0 +1,35 @@
+import keras
+from keras import backend as K
+from keras.preprocessing import image
+from keras.models import Model
+
+import tensorflow as tf
+from tensorflow.python.tools import freeze_graph, optimize_for_inference_lib
+
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+
+def get_model():
+    ### which parameter must match either with 'vgg16' or 'mobilenet'
+    """ takes a string which indicates the name of the desired model
+        returns the model pretrained with imagenet dataset """
+    vgg_model = keras.applications.VGG16(include_top=True, weights='imagenet')
+    vgg_model.layers.pop()
+    vgg_model.layers.pop()
+
+    inp = vgg_model.input
+    out = vgg_model.layers[-1].output
+
+    model = Model(inp, out)
+    return model
+
+def get_features(model, cropped_image):
+    """takes a pretrained model and a cropped image, returns features of the cropped image"""
+    x = image.img_to_array(cropped_image)
+    x = np.expand_dims(x, axis=0)
+    x = keras.applications.vgg16.preprocess_input(x)
+    features = model.predict(x)
+    return features

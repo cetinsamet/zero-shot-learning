203160752446bbf1a4b42d558e27a651bbb45d7f
Samet Çetin <e2036432@ceng.metu.edu.tr>
2018-09-12 12:37:12 +0300

    One hot encode only training classes -not the together with zero-shot classes-, output dimension of network is corrected (from 20 to 15), CRUCIAL IMPROVEMENTS

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ np.random.seed(123)
 import gzip
 import _pickle as cPickle
 import os
+from collections import Counter
 
 from sklearn.preprocessing import LabelEncoder, normalize
 from sklearn.neighbors import KDTree
@@ -47,34 +48,35 @@ def load_data():
     with gzip.GzipFile(DATAPATH, 'rb') as infile:
         data = cPickle.load(infile)
 
-    # SHUFFLE DATA
-    np.random.shuffle(data)
-    data_classnames, data_features = map(list, zip(*data))
-
     # ONE-HOT-ENCODE DATA
-    label_encoder = LabelEncoder()
-    label_encoder.fit(data_classnames)
+    label_encoder   = LabelEncoder()
+    label_encoder.fit(train_classes)
+
+    training_data = [instance for instance in data if instance[0] in train_classes]
+    zero_shot_data = [instance for instance in data if instance[0] not in train_classes]
+    # SHUFFLE TRAINING DATA
+    np.random.shuffle(training_data)
 
     ### SPLIT DATA FOR TRAINING
-    train_val_data = [(data_features[i], data_classnames[i]) for i in range(len(data)) if data[i][0] in train_classes]
-    train_size = 300
-    train_data, valid_data = list(), list()
-    for class_ in train_classes:
-        train_ct = 0
-
-        for data_ in train_val_data:
-            if data_[1] == class_:
-                if train_ct < train_size:
-                    train_data.append(data_)
-                    train_ct+=1
+    train_size  = 300
+    train_data  = list()
+    valid_data  = list()
+    for class_label in train_classes:
+        ct = 0
+        for instance in training_data:
+            if instance[0] == class_label:
+                if ct < train_size:
+                    train_data.append(instance)
+                    ct+=1
                     continue
-                valid_data.append(data_)
+                valid_data.append(instance)
 
     # SHUFFLE TRAINING AND VALIDATION DATA
     np.random.shuffle(train_data)
     np.random.shuffle(valid_data)
-    train_data = [(data[0], to_categorical(label_encoder.transform([data[1]]), num_classes=20))for data in train_data]
-    valid_data = [(data[0], to_categorical(label_encoder.transform([data[1]]), num_classes=20)) for data in valid_data]
+
+    train_data = [(instance[1], to_categorical(label_encoder.transform([instance[0]]), num_classes=15))for instance in train_data]
+    valid_data = [(instance[1], to_categorical(label_encoder.transform([instance[0]]), num_classes=15)) for instance in valid_data]
 
     # FORM X_TRAIN AND Y_TRAIN
     x_train, y_train    = zip(*train_data)
@@ -89,13 +91,8 @@ def load_data():
     x_valid = normalize(x_valid, norm='l2')
 
 
-    ### SPLIT DATA FOR ZERO-SHOT
-    zsl_data = [(data_features[i], data_classnames[i]) for i in range(len(data)) if data[i][0] in zsl_classes]
-    # SHUFFLE ZERO-SHOT DATA
-    np.random.shuffle(zsl_data)
-
     # FORM X_ZSL AND Y_ZSL
-    x_zsl, y_zsl = zip(*zsl_data)
+    y_zsl, x_zsl = zip(*zero_shot_data)
     x_zsl, y_zsl = np.squeeze(np.asarray(x_zsl)), np.squeeze(np.asarray(y_zsl))
     # L2 NORMALIZE X_ZSL
     x_zsl = normalize(x_zsl, norm='l2')
@@ -105,8 +102,9 @@ def load_data():
 
 
 def custom_kernel_init(shape):
-    class_vectors       = sorted(np.load(WORD2VECPATH), key=lambda x:x[0])
-    classnames, vectors = zip(*class_vectors)
+    class_vectors       = np.load(WORD2VECPATH)
+    training_vectors    = sorted([(label, vec) for (label, vec) in class_vectors if label in train_classes], key=lambda x: x[0])
+    classnames, vectors = zip(*training_vectors)
     vectors             = np.asarray(vectors, dtype=np.float)
     vectors             = vectors.T
     return vectors
@@ -153,13 +151,13 @@ def main():
     global zsl_classes
     with open('zsl_classes.txt', 'r') as infile:
         zsl_classes = [str.strip(line) for line in infile]
-
+    '''
     # ---------------------------------------------------------------------------------------------------------------- #
     # ---------------------------------------------------------------------------------------------------------------- #
     # SET HYPERPARAMETERS
 
     global NUM_CLASS, NUM_ATTR, EPOCH, BATCH_SIZE
-    NUM_CLASS = 20
+    NUM_CLASS = 15
     NUM_ATTR = 300
     BATCH_SIZE = 128
     EPOCH = 50
@@ -182,12 +180,12 @@ def main():
     zsl_model   = Model(inp, out)
     print(zsl_model.summary())
     save_keras_model(zsl_model, model_path=MODELPATH)
-
+    '''
     # ---------------------------------------------------------------------------------------------------------------- #
     # ---------------------------------------------------------------------------------------------------------------- #
     # EVALUATION OF ZERO-SHOT LEARNING PERFORMANCE
-    #(x_train, x_valid, x_zsl), (y_train, y_valid, y_zsl) = load_data()
-    #zsl_model = load_keras_model(model_path=MODELPATH)
+    (x_train, x_valid, x_zsl), (y_train, y_valid, y_zsl) = load_data()
+    zsl_model = load_keras_model(model_path=MODELPATH)
 
     class_vectors       = sorted(np.load(WORD2VECPATH), key=lambda x: x[0])
     classnames, vectors = zip(*class_vectors)
@@ -197,19 +195,24 @@ def main():
     tree        = KDTree(vectors)
     pred_zsl    = zsl_model.predict(x_zsl)
 
-    top5 = 0
+    top5, top3, top1 = 0, 0, 0
     for i, pred in enumerate(pred_zsl):
         pred            = np.expand_dims(pred, axis=0)
         dist_5, index_5 = tree.query(pred, k=5)
         pred_labels     = [classnames[index] for index in index_5[0]]
         true_label      = y_zsl[i]
-
         if true_label in pred_labels:
             top5 += 1
+        if true_label in pred_labels[:3]:
+            top3 += 1
+        if true_label in pred_labels[0]:
+            top1 += 1
 
     print()
     print("ZERO SHOT LEARNING SCORE")
-    print("-> Top-5 Accuracy: %.2f" % (top5/float(len(x_zsl))))
+    print("-> Top-5 Accuracy: %.2f" % (top5 / float(len(x_zsl))))
+    print("-> Top-3 Accuracy: %.2f" % (top3 / float(len(x_zsl))))
+    print("-> Top-1 Accuracy: %.2f" % (top1 / float(len(x_zsl))))
     return
 
 if __name__ == '__main__':
